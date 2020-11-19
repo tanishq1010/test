d1e9e9d71d39e1a34b0cb310aae448ba9e9a10eb
tanishq1010 <73070362+tanishq1010@users.noreply.github.com>
2020-11-19 16:25:11 +0530

    Add files via upload

--- goal_exam_extractor.py
+++ goal_exam_extractor.py
@@ -34,28 +34,28 @@ class Source(object):
         if response1.status_code == 200 and response1.json()["success"] == True:
             for goal in response1.json()["data"]:
                 _goal = goal["display_name"]
-                if _goal == 'CBSE' or _goal == 'Banking' or _goal == 'Engineering' or _goal == 'Medical':
+                if _goal == 'CBSE' or _goal == 'Banking' or _goal == 'Engineering' or _goal == 'Medical' or _goal == 'Insurance' or _goal == 'Defence' or _goal == 'SSC' or _goal == 'Teaching' or _goal=='Railways'  :
 
                     for exam in goal["exam"]:
                         # home = []
 
                         if str(exam["grade"]) != "None":
-                            home_data.append([_goal, str(exam["grade"]), str(exam["name"])])
+                            home_data.append([_goal, str(exam["grade"]), str(exam["name"]),str(exam["format_reference"])])
 
-        df = pd.DataFrame(data=home_data, columns=["Goal", "Grade", "Exam_name"])
+        df = pd.DataFrame(data=home_data, columns=["Goal", "Grade", "Exam_name","Format_refrence"])
 
-        df1 = df
-        df2 = df
-        list1 = [""] * len(df2)
-        df2["present only once"] = list1
-        for ind in df2.index:
-            df_new = df1.loc[df1['Exam_name'] == df2["Exam_name"][ind]]
-            if len(df_new) > 0:
-                df_new2 = df_new.loc[df_new['Goal'] == df2["Goal"][ind]]
-                if len(df_new2) == 1:
-                    df2["present only once"][ind] = str("yes")
-                else:
-                    df2["present only once"][ind] = str("no")
+        # df1 = df
+        # df2 = df
+        # list1 = [""] * len(df2)
+        # df2["present only once"] = list1
+        # for ind in df2.index:
+        #     df_new = df1.loc[df1['Exam_name'] == df2["Exam_name"][ind]]
+        #     if len(df_new) > 0:
+        #         df_new2 = df_new.loc[df_new['Goal'] == df2["Goal"][ind]]
+        #         if len(df_new2) == 1:
+        #             df2["present only once"][ind] = str("yes")
+        #         else:
+        #             df2["present only once"][ind] = str("no")
 
         return df
 
